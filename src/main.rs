9c94f5dc67cce38e7c313fcc9f80d1a5d930a35c
David LeGare <excaliburhissheath@gmail.com>
2018-11-02 00:53:30 -0500

    Notify game of new players

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+extern crate actix;
 extern crate actix_web;
+extern crate futures;
 extern crate log;
 extern crate log4rs;
 
 use actix_web::actix::*;
 use actix_web::fs::*;
-use actix_web::{server, ws, App, HttpRequest, Result};
 use actix_web::middleware::*;
+use actix_web::{server, ws, App, HttpRequest, Result};
+use futures::future::Future;
+use log::*;
 use std::collections::HashMap;
 
 fn main() {
@@ -15,11 +19,14 @@ fn main() {
 
     let game_addr = GameActor::new().start();
 
-    server::new(|| {
+    server::new(move || {
+        let game_addr = game_addr.clone();
         App::new()
             .middleware(Logger::default())
-            .resource("/ws/", |r| r.f(|req| ws::start(req, WebsocketActor)))
-            .resource("/", |r| r.f(index))
+            .resource("/ws/", move |resource| {
+                let game_addr = game_addr.clone();
+                resource.f(move |req| ws::start(req, WebsocketActor::new(game_addr.clone())))
+            }).resource("/", |resource| resource.f(index))
             .handler(
                 "/",
                 StaticFiles::new("./static").unwrap().show_files_listing(),
@@ -37,7 +44,7 @@ fn index(_req: &HttpRequest) -> Result<NamedFile> {
 
 #[derive(Debug, Default)]
 struct GameActor {
-    players: HashMap<usize, ()>,
+    players: HashMap<usize, Addr<WebsocketActor>>,
 }
 
 impl GameActor {
@@ -50,11 +57,36 @@ impl Actor for GameActor {
     type Context = Context<Self>;
 }
 
-enum ClientMessage {
-    PlayerJoined(usize),
+impl Handler<ClientMessage> for GameActor {
+    type Result = ();
+
+    fn handle(&mut self, message: ClientMessage, ctx: &mut Self::Context) -> Self::Result {
+        debug!("Incoming client message: {:?}", message);
+
+        match message {
+            ClientMessage::PlayerConnected(client_addr) => {
+                // TODO: Come up with a better system for generating player IDs.
+                let client_id = self.players.len();
+            }
+        }
+    }
 }
 
-struct WebsocketActor;
+#[derive(Debug, Message)]
+enum ClientMessage {
+    PlayerConnected(Addr<WebsocketActor>),
+}
+
+#[derive(Debug)]
+struct WebsocketActor {
+    game: Addr<GameActor>,
+}
+
+impl WebsocketActor {
+    fn new(game: Addr<GameActor>) -> Self {
+        WebsocketActor { game }
+    }
+}
 
 impl Actor for WebsocketActor {
     type Context = ws::WebsocketContext<Self>;
@@ -64,8 +96,20 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for WebsocketActor {
     fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
         match msg {
             ws::Message::Ping(msg) => ctx.pong(&msg),
-            ws::Message::Text(text) => println!("Received text frame: {:?}", text),
+
+            ws::Message::Text(text) => {
+                let send_future = self
+                    .game
+                    .send(ClientMessage::PlayerConnected(ctx.address()));
+
+                Arbiter::spawn(send_future.then(|result| {
+                    result.expect("Failed to send player connected message");
+                    Ok(())
+                }));
+            }
+
             ws::Message::Binary(bin) => println!("Received binary frame: {:?}", bin),
+
             _ => (),
         }
     }
