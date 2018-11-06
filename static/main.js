8fa124abd6695eaeda70631181d7bf24bd5fcede
David LeGare <excaliburhissheath@gmail.com>
2018-11-05 23:03:08 -0600

    Move the script tag for main.js to end of document

--- static/main.js
+++ static/main.js
@@ -1,29 +1,27 @@
-window.onload = function (event) {
-    let app = new Vue({
-        el: '#app',
+let app = new Vue({
+    el: '#app',
 
-        data: {
-            socket: null,
-            players: [],
-        },
+    data: {
+        socket: null,
+        players: [],
+    },
 
-        created: function () {
-            this.socket = new WebSocket('ws://localhost:8088/ws/');
+    created: function () {
+        this.socket = new WebSocket('ws://localhost:8088/ws/');
 
-            this.socket.onmessage = (event) => {
-                let payload = JSON.parse(event.data);
-                console.log('Received message:', payload);
+        this.socket.onmessage = (event) => {
+            let payload = JSON.parse(event.data);
+            console.log('Received message:', payload);
 
-                // If the payload updates the list of players, apply the update
-                // to the local state.
-                if (payload.players != null) {
-                    this.players = payload.players;
-                }
-            }
-
-            this.socket.onclose = (event) => {
-                console.log('Disconnected from server:', event);
+            // If the payload updates the list of players, apply the update
+            // to the local state.
+            if (payload.players != null) {
+                this.players = payload.players;
             }
         }
-    });
-}
+
+        this.socket.onclose = (event) => {
+            console.log('Disconnected from server:', event);
+        }
+    }
+});
